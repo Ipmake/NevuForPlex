ad04ad38432b861b11a0730038ae9a6e54c4029f
Ipmake <niko.denis727@gmail.com>
2025-08-07 01:50:30 +0200

    Fixed issue related to client cookies on images

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -496,6 +496,10 @@ app.use('/dynproxy/*', (req, res) => {
     const url = req.originalUrl.split('/dynproxy')[1];
     if (!url) return res.status(400).send('Bad request');
 
+    // strip cookies from the request
+    req.headers.cookie = '';
+    req.headers['x-forwarded-for'] = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+
     proxy.web(req, res, { target: `${process.env.PLEX_SERVER}${url}` }, (err) => {
         console.error('Proxy error:', err);
         res.status(500).send('Proxy error');
